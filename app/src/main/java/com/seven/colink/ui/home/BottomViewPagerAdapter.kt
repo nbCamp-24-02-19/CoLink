9d1c39241fdd63af7c7ae9f096a7c8ee3bebf323
kgdrjsen <gundoo3333@gmail.com>
2024-02-28 00:40:50 +0900

    remove: delete unnecessary xml

--- app/src/main/java/com/seven/colink/ui/home/BottomViewPagerAdapter.kt
+++ app/src/main/java/com/seven/colink/ui/home/BottomViewPagerAdapter.kt
@@ -1,18 +1,9 @@
 package com.seven.colink.ui.home
 
-import android.view.LayoutInflater
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.seven.colink.databinding.ItemHomeBottomViewpagerBinding
 import com.seven.colink.ui.home.child.HomeProjectFragment
 import com.seven.colink.ui.home.child.HomeStudyFragment
-import com.seven.colink.ui.main.MainActivity
-import org.checkerframework.checker.nullness.qual.NonNull
 
 class BottomViewPagerAdapter (frag : Fragment) : FragmentStateAdapter(frag) {
 //    private lateinit var bottomViewPagerAdapter: BottomViewPagerAdapter
