c8158b9a678710e391d29fcba187f802c570fff6
kgdrjsen <gundoo3333@gmail.com>
2024-02-28 00:46:26 +0900

    remove: delete unnecessary import

--- app/src/main/java/com/seven/colink/ui/home/HomeMainAdapter.kt
+++ app/src/main/java/com/seven/colink/ui/home/HomeMainAdapter.kt
@@ -5,7 +5,6 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
-import com.seven.colink.databinding.ItemHomeBottomViewpagerBinding
 import com.seven.colink.databinding.ItemHomeHeaderBinding
 import com.seven.colink.databinding.ItemHomeTopViewpagerBinding
 
