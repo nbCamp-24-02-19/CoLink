e7d1c5450758e518c4862585a83e68dd349a2491
kgdrjsen <gundoo3333@gmail.com>
2024-02-28 20:11:50 +0900

    feat: infinity scroll viewpager

--- app/src/main/java/com/seven/colink/ui/home/HomeFragment.kt
+++ app/src/main/java/com/seven/colink/ui/home/HomeFragment.kt
@@ -1,11 +1,15 @@
 package com.seven.colink.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.ViewPager2
 import com.seven.colink.R
 import com.seven.colink.databinding.FragmentHomeBinding
 import com.seven.colink.util.dialog.setDialog
@@ -18,6 +22,16 @@ class HomeFragment : Fragment() {
     private val mainAdapter by lazy { HomeMainAdapter() }
     private lateinit var bottomAdapter : BottomViewPagerAdapter
     private val topAdapter by lazy { TopViewPagerAdapter() }
+
+    private val tolList = mutableListOf<TopItems>(
+        TopItems(R.drawable.img_dialog_project,"팀이름","날짜","타이틀"),
+        TopItems(R.drawable.img_dialog_study,"팀","yyyy-mm","title2"),
+        TopItems(R.drawable.img_user_grade,"team이름","yyyy-mm","title2"),
+        TopItems(R.drawable.img_dialog_study,"이름","yyyy-mm","title2"),
+        TopItems(R.drawable.img_dialog_project,"이거","yyyy-mm","title2"),
+        TopItems(R.drawable.img_dialog_study,"뭐라고","yyyy-mm","title2"),
+        TopItems(R.drawable.img_temporary,"할까","yyyy-mm","title2")
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -37,6 +51,9 @@ class HomeFragment : Fragment() {
     }
 
     private fun initViews() {
+        topAdapter.submitList(tolList)          // dummy data
+
+
         val homeItem : MutableList<HomeAdapterItems> = mutableListOf(
             HomeAdapterItems.TopView(topAdapter),
             HomeAdapterItems.Header("그룹 추천")
@@ -49,23 +66,22 @@ class HomeFragment : Fragment() {
         mainAdapter.submitList(homeItem)
     }
 
-
     override fun onResume() {
         super.onResume()
-        requireContext().setDialog(
-            title = "안녕하세요",
-            message = "'다이얼로그'가 잘 작성 되는지 '확인' 해보는 다이얼로그",
-            image = R.drawable.img_dialog_project,
-            confirmAction = { dialog -> dialog.dismiss()
-                            requireContext().setDialog(
-                                title = "반갑고",
-                                message = "안녕하고",
-                                confirmAction = {dialog -> dialog.dismiss()
-                                                requireContext().setLevelDialog { Unit }.show()},
-                                cancelAction = {dialog -> dialog.dismiss()}
-                            ).show()},
-            cancelAction = {dialog -> dialog.dismiss()}
-        ).show()
+//        requireContext().setDialog(
+//            title = "안녕하세요",
+//            message = "'다이얼로그'가 잘 작성 되는지 '확인' 해보는 다이얼로그",
+//            image = R.drawable.img_dialog_project,
+//            confirmAction = { dialog -> dialog.dismiss()
+//                            requireContext().setDialog(
+//                                title = "반갑고",
+//                                message = "안녕하고",
+//                                confirmAction = {dialog -> dialog.dismiss()
+//                                                requireContext().setLevelDialog { Unit }.show()},
+//                                cancelAction = {dialog -> dialog.dismiss()}
+//                            ).show()},
+//            cancelAction = {dialog -> dialog.dismiss()}
+//        ).show()
     }
     override fun onDestroyView() {
         super.onDestroyView()
--- app/src/main/java/com/seven/colink/ui/home/child/HomeStudyFragment.kt
+++ app/src/main/java/com/seven/colink/ui/home/child/HomeStudyFragment.kt
@@ -5,15 +5,141 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import coil.load
 import com.seven.colink.R
+import com.seven.colink.databinding.FragmentHomeBinding
+import com.seven.colink.databinding.FragmentHomeStudyBinding
+import com.seven.colink.ui.home.BottomItems
 
 class HomeStudyFragment : Fragment() {
-
+    private var _binding: FragmentHomeStudyBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_study, container, false)
+    ): View {
+        _binding = FragmentHomeStudyBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+    }
+
+    private fun initViews(){
+        bottomViews()
+    }
+
+    private fun bottomViews(){
+        val bottomList = mutableListOf<BottomItems>(
+            BottomItems("Study","이거 뭘로 정하지","설명을 뭐로하지 일단 설명을 길게 길게 길게 길게 길게길게길게길게","AOS","초심자"
+            ,R.drawable.img_dialog_project,false,false),
+            BottomItems("Study","저거 뭘로 정하지","설명","Kotlin","학생"
+                ,R.drawable.img_user_grade,false,false),
+            BottomItems("Study","그거 뭘로 정하지","뭐로하지","Java","없음"
+                ,R.drawable.img_dialog_study,false,false),
+            BottomItems("Study","이곳 뭘로 정하지","설명을 해야하나","C언어","중급자"
+                ,R.drawable.img_dialog_project,true,true),
+            BottomItems("Study","지금 뭘로 정하지","뭘 해야하누","AOS","초급자"
+                ,R.drawable.img_dialog_project,true,true),
+        )
+        with(binding){
+            layStudyBottom1.tvHomeBottomProject.visibility = View.INVISIBLE
+            layStudyBottom2.tvHomeBottomProject.visibility = View.INVISIBLE
+            layStudyBottom3.tvHomeBottomProject.visibility = View.INVISIBLE
+            layStudyBottom4.tvHomeBottomProject.visibility = View.INVISIBLE
+            layStudyBottom5.tvHomeBottomProject.visibility = View.INVISIBLE
+        }
+
+        with(binding.layStudyBottom1) {
+            val item = bottomList[0]
+            tvHomeBottomStudy.visibility = View.VISIBLE
+            tvHomeBottomStudy.text = item.typeId
+            tvHomeBottomTitle.text = item.title
+            tvHomeBottomDes.text = item.des
+            tvHomeBottomKind.text = item.kind
+            tvHomeBottomLv.text = item.lv
+            ivHomeBottomThumubnail.load(item.img)
+            if (item.blind && item.complete) {
+                viewHomeBottomBlind.visibility = View.VISIBLE
+                tvHomeBottomBlind.visibility = View.VISIBLE
+            }else {
+                viewHomeBottomBlind.visibility = View.INVISIBLE
+                tvHomeBottomBlind.visibility = View.INVISIBLE
+            }
+        }
+
+        with(binding.layStudyBottom2) {
+            val item = bottomList[1]
+            tvHomeBottomStudy.visibility = View.VISIBLE
+            tvHomeBottomStudy.text = item.typeId
+            tvHomeBottomTitle.text = item.title
+            tvHomeBottomDes.text = item.des
+            tvHomeBottomKind.text = item.kind
+            tvHomeBottomLv.text = item.lv
+            ivHomeBottomThumubnail.load(item.img)
+            if (item.blind && item.complete) {
+                viewHomeBottomBlind.visibility = View.VISIBLE
+                tvHomeBottomBlind.visibility = View.VISIBLE
+            }else {
+                viewHomeBottomBlind.visibility = View.INVISIBLE
+                tvHomeBottomBlind.visibility = View.INVISIBLE
+            }
+        }
+
+        with(binding.layStudyBottom3) {
+            val item = bottomList[2]
+            tvHomeBottomStudy.visibility = View.VISIBLE
+            tvHomeBottomStudy.text = item.typeId
+            tvHomeBottomTitle.text = item.title
+            tvHomeBottomDes.text = item.des
+            tvHomeBottomKind.text = item.kind
+            tvHomeBottomLv.text = item.lv
+            ivHomeBottomThumubnail.load(item.img)
+            if (item.blind && item.complete) {
+                viewHomeBottomBlind.visibility = View.VISIBLE
+                tvHomeBottomBlind.visibility = View.VISIBLE
+            }else {
+                viewHomeBottomBlind.visibility = View.INVISIBLE
+                tvHomeBottomBlind.visibility = View.INVISIBLE
+            }
+        }
+
+        with(binding.layStudyBottom4) {
+            val item = bottomList[3]
+            tvHomeBottomStudy.visibility = View.VISIBLE
+            tvHomeBottomStudy.text = item.typeId
+            tvHomeBottomTitle.text = item.title
+            tvHomeBottomDes.text = item.des
+            tvHomeBottomKind.text = item.kind
+            tvHomeBottomLv.text = item.lv
+            ivHomeBottomThumubnail.load(item.img)
+            if (item.blind && item.complete) {
+                viewHomeBottomBlind.visibility = View.VISIBLE
+                tvHomeBottomBlind.visibility = View.VISIBLE
+            }else {
+                viewHomeBottomBlind.visibility = View.INVISIBLE
+                tvHomeBottomBlind.visibility = View.INVISIBLE
+            }
+        }
+
+        with(binding.layStudyBottom5) {
+            val item = bottomList[4]
+            tvHomeBottomStudy.visibility = View.VISIBLE
+            tvHomeBottomStudy.text = item.typeId
+            tvHomeBottomTitle.text = item.title
+            tvHomeBottomDes.text = item.des
+            tvHomeBottomKind.text = item.kind
+            tvHomeBottomLv.text = item.lv
+            ivHomeBottomThumubnail.load(item.img)
+            if (item.blind && item.complete) {
+                viewHomeBottomBlind.visibility = View.VISIBLE
+                tvHomeBottomBlind.visibility = View.VISIBLE
+            }else {
+                viewHomeBottomBlind.visibility = View.INVISIBLE
+                tvHomeBottomBlind.visibility = View.INVISIBLE
+            }
+        }
     }
 }
