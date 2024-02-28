de2b9d0c14194bf3228de2cb27b3c6c86ce029b2
Guri999 <116724657+Guri999@users.noreply.github.com>
2024-02-29 00:43:13 +0900

    feat: "✨로그인 구현, 프로그레스바 구현"

--- app/src/main/java/com/seven/colink/ui/sign/signin/viewmodel/SignInViewModel.kt
+++ app/src/main/java/com/seven/colink/ui/sign/signin/viewmodel/SignInViewModel.kt
@@ -1,14 +1,36 @@
 package com.seven.colink.ui.sign.signin.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.seven.colink.domain.repository.AuthRepository
+import com.seven.colink.util.status.DataResultStatus
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class SignInViewModel @Inject constructor(
     private val authRepository: AuthRepository,
 ):ViewModel() {
+    private val _entryType = MutableStateFlow(false)
 
-    
+    val entryType: StateFlow<Boolean> = _entryType
+
+    private val _result = MutableSharedFlow<DataResultStatus>()
+    val result = _result
+    init {
+        viewModelScope.launch {
+            _entryType.value = authRepository.getCurrentUser() == DataResultStatus.SUCCESS
+        }
+    }
+    suspend fun signIn(email: String, password: String) {
+        _result.emit(authRepository.signIn(email, password))
+    }
+
+    fun isSignIn(result: DataResultStatus) {
+        _entryType.value = result == DataResultStatus.SUCCESS
+    }
 }
