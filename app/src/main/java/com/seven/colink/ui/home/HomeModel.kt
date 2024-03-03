fca33301b6e79a8ab8bd79ba071b309066224129
kgdrjsen <gundoo3333@gmail.com>
2024-03-03 23:39:48 +0900

    feat: observe

--- app/src/main/java/com/seven/colink/ui/home/HomeModel.kt
+++ app/src/main/java/com/seven/colink/ui/home/HomeModel.kt
@@ -1,23 +1,31 @@
 package com.seven.colink.ui.home
 
 import com.seven.colink.ui.home.adapter.TopViewPagerAdapter
+import com.seven.colink.util.status.GroupType
+import com.seven.colink.util.status.ProjectStatus
 
 data class TopItems (
-    val img : Int,
-    val team : String,
-    val date : String,
-    val title : String
+    val img : String?,
+    val team : String?,
+    val date : String?,
+    val title : String?,
+    val key : String?
 )
 
 data class BottomItems (
-    val typeId : String,
-    val title : String,
-    val des : String,
-    val kind : String,
-    val lv : String,
-    val img : Int,
-    val blind : Boolean,
-    val complete : Boolean
+//    val typeId : Int?,
+    val typeId : GroupType?,
+    val title : String?,
+    val des : String?,
+//    val kind : String?,
+    val kind : List<String>?,
+    val lv : String?,
+    val img : String?,
+    val key : String?,
+//    val blind : Boolean,
+//    val complete : Boolean
+    val blind : ProjectStatus?,
+    val complete : ProjectStatus?
 )
 
 sealed class HomeAdapterItems {
--- app/src/main/java/com/seven/colink/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/seven/colink/ui/home/HomeViewModel.kt
@@ -1,38 +1,72 @@
 package com.seven.colink.ui.home
 
+import android.content.Context
+import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.seven.colink.domain.entity.PostEntity
 import com.seven.colink.domain.repository.PostRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel@Inject constructor(
+class HomeViewModel @Inject constructor(
     private val postRepository: PostRepository,
+//    private val mContext: Context
 ) : ViewModel() {
-    private val _topItems : MutableLiveData<List<TopItems>> = MutableLiveData(mutableListOf())
-    private val _bottomItems : MutableLiveData<List<BottomItems>> = MutableLiveData(mutableListOf())
-    val topItems : LiveData<List<TopItems>> get() = _topItems
-    val bottomItems : LiveData<List<BottomItems>> get() = _bottomItems
+    private val _topItems: MutableLiveData<List<TopItems>> = MutableLiveData(mutableListOf())
+    private val _bottomItems: MutableLiveData<List<BottomItems>> = MutableLiveData(mutableListOf())
+    val topItems: LiveData<List<TopItems>> get() = _topItems
+    val bottomItems: LiveData<List<BottomItems>> get() = _bottomItems
 
-    suspend fun getTopItems(string: String) {
-        var getTopItemList : MutableList<TopItems> = mutableListOf()
-        var t :MutableLiveData<List<PostEntity>> = MutableLiveData()
+    fun getTopItems(num : Int) {
+        var getTopItemList: MutableList<TopItems> = mutableListOf()
 
         viewModelScope.launch {
             getTopItemList.clear()
-            val tt = t.value
-            val repository = postRepository.getPost(string)
+            val repository = postRepository.getRecentPost(num)
             kotlin.runCatching {
-                repository.getOrNull()?.
+                val item = repository.getOrNull(num)
+                repository.forEach {
+                    var topRecentItem = TopItems(item?.imageUrl,item?.recruitInfo,item?.startDate,
+                        item?.title,item?.key)
+                    getTopItemList.add(topRecentItem)
+                }
+                _topItems.value = getTopItemList
+
+            }.onFailure { exception ->
+                Log.e("HomeViewModel","#aaa error $exception")
+//                Toast.makeText(mContext,"다음에 다시 시도해주세요.", Toast.LENGTH_SHORT).show()
             }
-
-
         }
     }
 
+    fun getBottomItems(num: Int) {
+        var getBottomItemList: MutableList<BottomItems> = mutableListOf()
+
+        viewModelScope.launch {
+            getBottomItemList.clear()
+            val repository = postRepository.getRecentPost(num)
+            kotlin.runCatching {
+                val item = repository.getOrNull(num)
+
+                repository.forEach {
+                    val lv = item?.recruit?.forEach { lv ->
+                        lv.level
+                    }.toString()
+
+                    var bottomRecentItem = BottomItems(item?.groupType,item?.title,item?.description
+                    ,item?.tags,lv,item?.imageUrl,item?.key,item?.status,item?.status)
+                    getBottomItemList.add(bottomRecentItem)
+                }
+                _bottomItems.value = getBottomItemList
+            }.onFailure { exception ->
+                Log.e("HomeViewModel", "#aaa error $exception")
+//                Toast.makeText(mContext, "다음에 다시 시도해주세요.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 }
