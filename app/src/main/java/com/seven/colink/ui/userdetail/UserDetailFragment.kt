73570d94acf69aa708dd0aa77d64c2ddf73f3472
PEPTIDE707 <gpflsans@gmail.com>
2024-03-05 10:36:48 +0900

    fix

--- app/src/main/java/com/seven/colink/ui/userdetail/UserDetailFragment.kt
+++ app/src/main/java/com/seven/colink/ui/userdetail/UserDetailFragment.kt
@@ -8,10 +8,14 @@ import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.seven.colink.R
 import com.seven.colink.databinding.FragmentUserDetailBinding
+import com.seven.colink.ui.mypage.MyPageItem
+import com.seven.colink.ui.mypage.MyPageSkilItemManager
+import com.seven.colink.util.skillCategory
 
 class UserDetailFragment : Fragment() {
     private lateinit var binding: FragmentUserDetailBinding
     private lateinit var viewModel: UserDetailViewModel
+    private lateinit var adapter: UserSkillAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?, ): View? {
 
@@ -20,7 +24,7 @@ class UserDetailFragment : Fragment() {
 
         viewModel.userDetails.observe(viewLifecycleOwner) {userDetails ->
             updateUI(userDetails)
-
+            adapter.changeDataset(userDetails.userSkill?.map { UserSkillItem(it,UserSkillItemManager.addItem(it)) }?: emptyList())
         }
 
 
@@ -29,6 +33,13 @@ class UserDetailFragment : Fragment() {
 
     private fun updateUI(user: UserDetailModel){
         binding.tvUserdetailName.text = user.userName
+        binding.tvUserdetailAboutMe.text = user.userInfo
+        binding.tvUserdetailSpecialty.text = user.userMainSpecialty
+        binding.tvUserdetailScore.text = user.userscore.toString()
+    }
+
+    private fun userSkill(){
+
     }
 
 }
