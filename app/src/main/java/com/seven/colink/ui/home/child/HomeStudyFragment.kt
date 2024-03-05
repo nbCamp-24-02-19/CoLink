3ea31af0420e7f4d0c02f14bdf4eb7a0a1c6230c
kgdrjsen <gundoo3333@gmail.com>
2024-03-05 15:13:40 +0900

    feat: infinity scroll complete

--- app/src/main/java/com/seven/colink/ui/home/child/HomeStudyFragment.kt
+++ app/src/main/java/com/seven/colink/ui/home/child/HomeStudyFragment.kt
@@ -1,6 +1,7 @@
 package com.seven.colink.ui.home.child
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -12,6 +13,7 @@ import coil.load
 import com.seven.colink.databinding.FragmentHomeStudyBinding
 import com.seven.colink.ui.home.HomeViewModel
 import com.seven.colink.ui.post.content.PostContentActivity
+import com.seven.colink.util.status.GroupType
 import com.seven.colink.util.status.ProjectStatus
 import kotlinx.coroutines.launch
 
@@ -57,50 +59,56 @@ class HomeStudyFragment : Fragment() {
             }
 
             bottomLayout.apply {
-//                if (bottom.typeId == GroupType.STUDY) {
-//                    if (homeViewModel._bottomItems.value?.size!! < 5){
-//                        homeViewModel.getBottomItems(5)
-//                        if (homeViewModel._bottomItems.value?.size!! > 5){
-//                            val currentList = homeViewModel._bottomItems.value?.toMutableList()
-//                            currentList?.let {
-//                                if (it.isNotEmpty()) {
-//                                    it.removeAt(it.size -1)
-//                                    homeViewModel._bottomItems.value = it
-//                                }
-//                            }
-//                        }
-//                    }else if (homeViewModel._bottomItems.value?.size!! == 5) {
-                        tvHomeBottomStudy.visibility = View.VISIBLE
-                        tvHomeBottomProject.visibility = View.INVISIBLE
-                        tvHomeBottomTitle.text = bottom.title
-                        tvHomeBottomDes.text = bottom.des
-                        tvHomeBottomKind.text = bottom.kind?.toString()
-                        viewHomeBottomDivider.visibility = View.INVISIBLE
-                        tvHomeBottomLv.visibility = View.INVISIBLE
-                        ivHomeBottomThumubnail.load(bottom.img)
-                        if (bottom.blind == ProjectStatus.END) {
-                            viewHomeBottomBlind.visibility = View.VISIBLE
-                            tvHomeBottomBlind.visibility = View.VISIBLE
-                        } else {
-                            viewHomeBottomBlind.visibility = View.INVISIBLE
-                            tvHomeBottomBlind.visibility = View.INVISIBLE
-                        }
-                        layBottom.setOnClickListener {
-                            lifecycleScope.launch {
-                                val key = bottom.key
-                                if (key != null) {
-                                    val intent = PostContentActivity.newIntent(
-                                        requireContext(),
-                                        key
-                                    )
-                                    startActivity(intent)
-                                } else {
-                                    Toast.makeText(requireContext(), "다음에 다시 시도해주세요.", Toast.LENGTH_SHORT)
-                                        .show()
+                if (bottom.typeId == GroupType.STUDY) {
+                    while (true){
+                        if (homeViewModel._bottomItems.value?.size!! < 5){
+                            homeViewModel.getBottomItems(5)
+                            if (homeViewModel._bottomItems.value?.size!! > 5){
+                                val currentList = homeViewModel._bottomItems.value?.toMutableList()
+                                currentList?.let {
+                                    it.removeAt(it.size -1)
+                                    homeViewModel._bottomItems.value = it
                                 }
                             }
-//                        }
-//                    }
+                        }else if (homeViewModel._bottomItems.value?.size!! == 5) {
+                            break
+                        }
+                    }
+
+                     if (homeViewModel._bottomItems.value?.size!! == 5) {
+                            tvHomeBottomStudy.visibility = View.VISIBLE
+                            tvHomeBottomProject.visibility = View.INVISIBLE
+                            tvHomeBottomTitle.text = bottom.title
+                         Log.d("Study","#eee study = $bottom")
+                            Log.d("Study","#eee 3번째 들어오는거 맞아? ${bottom.title}")
+
+                            tvHomeBottomDes.text = bottom.des
+                            tvHomeBottomKind.text = bottom.kind?.toString()
+                            viewHomeBottomDivider.visibility = View.INVISIBLE
+                            tvHomeBottomLv.visibility = View.INVISIBLE
+                            ivHomeBottomThumubnail.load(bottom.img)
+
+                            if (bottom.blind == ProjectStatus.END) {
+                            viewHomeBottomBlind.visibility = View.VISIBLE
+                            tvHomeBottomBlind.visibility = View.VISIBLE
+                            } else {
+                            viewHomeBottomBlind.visibility = View.INVISIBLE
+                            tvHomeBottomBlind.visibility = View.INVISIBLE
+                            }
+
+                            layBottom.setOnClickListener {
+                                lifecycleScope.launch {
+                                    val key = bottom.key
+                                    if (key != null) {
+                                        val intent = PostContentActivity.newIntent(requireContext(),key)
+                                        startActivity(intent)
+                                    } else {
+                                        Toast.makeText(requireContext(), "다음에 다시 시도해주세요.", Toast.LENGTH_SHORT)
+                                            .show()
+                                    }
+                                }
+                            }
+                     }
                 }
             }
         }
