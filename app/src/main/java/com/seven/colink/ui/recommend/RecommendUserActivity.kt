25f02a73c57fd4fdd0c1eed62540879bcdcdce29
Guri999 <116724657+Guri999@users.noreply.github.com>
2024-03-05 20:01:12 +0900

    "feat: ✨ 유저 추천 페이지 UI"

---
+++ app/src/main/java/com/seven/colink/ui/recommend/RecommendUserActivity.kt
@@ -0,0 +1,12 @@
+package com.seven.colink.ui.recommend
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.seven.colink.R
+
+class RecommendUserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recommand_user)
+    }
+}
