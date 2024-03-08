a2e6ffbc1f0062b23d0869ce22bee49bfaf93c3f
PEPTIDE707 <gpflsans@gmail.com>
2024-03-08 19:37:57 +0900

    fix : mypage

--- app/src/main/java/com/seven/colink/ui/userdetail/UserPostItemManager.kt
+++ app/src/main/java/com/seven/colink/ui/userdetail/UserPostItemManager.kt
@@ -4,12 +4,12 @@ import com.seven.colink.ui.mypage.MyPostItem
 
 class UserPostItemManager {
     private val UserPostItemList = listOf(
-        MyPostItem.MyPagePostItem("참여중","코링","현재"),
-        MyPostItem.MyPagePostItem("참여중","코링","현재"),
-        MyPostItem.MyPagePostItem("참여중","코링","현재"),
-        MyPostItem.MyPagePostItem("중도하차","코링","현재"),
-        MyPostItem.MyPagePostItem("완료","코링","현재"),
-        MyPostItem.MyPageStudyItem("참여중","MVVM 공부","현재"),
+        MyPostItem.MyPagePostItem("참여중","코링","현재",""),
+        MyPostItem.MyPagePostItem("참여중","코링","현재",""),
+        MyPostItem.MyPagePostItem("참여중","코링","현재",""),
+        MyPostItem.MyPagePostItem("중도하차","코링","현재",""),
+        MyPostItem.MyPagePostItem("완료","코링","현재",""),
+        MyPostItem.MyPageStudyItem("참여중","MVVM 공부","현재",""),
     )
 
     fun getItemAll() : List<MyPostItem> = UserPostItemList
