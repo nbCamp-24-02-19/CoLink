34e762f06e5c0948556f26848351bb5249136eef
kgdrjsen <gundoo3333@gmail.com>
2024-03-08 10:07:33 +0900

    merge

--- app/src/main/java/com/seven/colink/ui/home/HomeFragment.kt
+++ app/src/main/java/com/seven/colink/ui/home/HomeFragment.kt
@@ -1,8 +1,10 @@
 package com.seven.colink.ui.home
 
 import android.content.Context
+import android.graphics.Rect
 import android.os.Bundle
 import android.util.DisplayMetrics
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,6 +13,11 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.CompositePageTransformer
+import androidx.viewpager2.widget.MarginPageTransformer
+import androidx.viewpager2.widget.ViewPager2
+import coil.size.Dimension
 import com.seven.colink.R
 import com.seven.colink.databinding.FragmentHomeBinding
 import com.seven.colink.ui.home.adapter.BottomViewPagerAdapter
@@ -19,6 +26,7 @@ import com.seven.colink.ui.home.adapter.TopViewPagerAdapter
 import com.seven.colink.ui.post.register.PostActivity
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
+import kotlin.math.abs
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
@@ -64,22 +72,21 @@ class HomeFragment : Fragment() {
         }
 
         bottomAdapter = BottomViewPagerAdapter(this)
+        binding.vpHome.adapter = bottomAdapter
         previewViewPager()
     }
 
     private fun previewViewPager(){
-        binding.vpHome.adapter = bottomAdapter
-        binding.vpHome.offscreenPageLimit = 2
+        binding.vpHome.offscreenPageLimit = 1
         val pageMargin = resources.getDimensionPixelOffset(R.dimen.page_home_margin)
         val pagerOffset = resources.getDimensionPixelOffset(R.dimen.offset_home_between_pages)
         val screenWidth = resources.displayMetrics.widthPixels
-        val screen = pixelToDp(screenWidth,requireContext())
-        val offsetPx = screen - (pageMargin + pagerOffset) - (pageMargin/2)
 
-        binding.vpHome.setPageTransformer { page, position ->
-            page.translationX = position * (-offsetPx)
-        }
     }
+
+
+
+
 
     private fun pixelToDp(px: Int, context: Context) : Float {
         return px / ((context.resources.displayMetrics.densityDpi.toFloat()) / DisplayMetrics.DENSITY_DEFAULT)
