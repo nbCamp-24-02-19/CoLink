3a8492cd94d497e133acb10f32a2612aee737ae0
kgdrjsen <gundoo3333@gmail.com>
2024-03-15 19:37:08 +0900

    feat: get receive key for productpromotion activity

--- app/src/main/java/com/seven/colink/ui/promotion/ProductPromotionActivity.kt
+++ app/src/main/java/com/seven/colink/ui/promotion/ProductPromotionActivity.kt
@@ -2,7 +2,10 @@ package com.seven.colink.ui.promotion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.seven.colink.R
 import com.seven.colink.databinding.ActivityProductPromotionBinding
+import com.seven.colink.util.Constants
 
 class ProductPromotionActivity : AppCompatActivity() {
 
@@ -11,6 +14,32 @@ class ProductPromotionActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        initView()
+    }
 
+    private fun initView() = with(binding) {
+        val receiveKey = intent.getStringExtra(Constants.EXTRA_ENTITY_KEY)
+        if (receiveKey != null) {
+            if (receiveKey.startsWith("PRD_")){
+                val frag = ProductPromotionFragment()
+                val bundle = Bundle()
+                bundle.putString(Constants.EXTRA_ENTITY_KEY,receiveKey)
+                frag.arguments = bundle
+                setFragment(frag)
+            }else{
+                val frag = ProductPromotionEditFragment()
+                val bundle = Bundle()
+                bundle.putString(Constants.EXTRA_ENTITY_KEY,receiveKey)
+                frag.arguments = bundle
+                setFragment(frag)
+            }
+        }
+    }
+
+    private fun setFragment(frag : Fragment) {
+        val tran = supportFragmentManager.beginTransaction()
+        tran.replace(R.id.frame_product_promotion, frag)
+        tran.addToBackStack("")
+        tran.commit()
     }
 }
