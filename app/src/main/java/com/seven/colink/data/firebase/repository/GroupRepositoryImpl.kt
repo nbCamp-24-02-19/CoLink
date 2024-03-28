f36a2ac8c33606b2dd59e4ed32899c48c85280b0
Guri999 <116724657+Guri999@users.noreply.github.com>
2024-03-28 15:51:20 +0900

    design: centerCrop, grade 컨버트

--- app/src/main/java/com/seven/colink/data/firebase/repository/GroupRepositoryImpl.kt
+++ app/src/main/java/com/seven/colink/data/firebase/repository/GroupRepositoryImpl.kt
@@ -128,7 +128,7 @@ class GroupRepositoryImpl @Inject constructor(
         }
     }.map { data ->
         data.filter {
-            it.status != ProjectStatus.RECRUIT && it.evaluateMember?.contains(auth.currentUser?.uid)?.not()?: true
+            it.status != ProjectStatus.RECRUIT && it.evaluateMember?.contains(auth.currentUser?.uid)?.not()?: true && it.memberIds.size != 1
         }.takeIf {
             it.isNotEmpty()
         }
